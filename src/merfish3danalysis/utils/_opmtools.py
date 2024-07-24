260f89964a059945d692c2898eee6e563e9bcc2a
dpshepherd <dpshepherd@gmail.com>
2024-07-23 20:46:08 -0700

    Switch to pycudadecon

--- src/merfish3danalysis/utils/_opmtools.py
+++ src/merfish3danalysis/utils/_opmtools.py
@@ -5,6 +5,29 @@ import numpy as np
 from numpy.typing import ArrayLike
 import gc
 
+def pad_to_nearest_16_z_reflect(image):
+    def next_multiple_of_16(x):
+        return (x + 15) // 16 * 16
+    
+    z, y, x = image.shape
+    
+    new_z = next_multiple_of_16(z)
+    pad_z = new_z - z
+    
+    # Distribute padding evenly on both sides
+    pad_z_before = pad_z // 2
+    pad_z_after = pad_z - pad_z_before
+    
+    # Padding configuration for numpy.pad
+    pad_width = ((pad_z_before, pad_z_after), (0, 0), (0, 0))
+    
+    padded_image = np.pad(image, pad_width, mode='reflect')
+    
+    return padded_image, pad_z_before, pad_z_after
+
+def remove_padding_z(image, pad_z_before, pad_z_after):
+    return image[pad_z_before:-pad_z_after,:]
+
 def chunked_orthogonal_deskew(oblique_image: ArrayLike,
                             psf_data: ArrayLike,
                             chunk_size: int = 15000,
@@ -15,7 +38,7 @@ def chunked_orthogonal_deskew(oblique_image: ArrayLike,
                             camera_qe: float = 0.9,
                             z_downsample_level = 2,
                             perform_decon: bool = True,
-                            decon_iterations: int = 100,
+                            decon_iterations: int = 10,
                             decon_chunks: int = 256) -> ArrayLike:
 
     output_shape = deskew_shape_estimator(oblique_image.shape)
@@ -67,25 +90,60 @@ def chunked_orthogonal_deskew(oblique_image: ArrayLike,
         raw_data = ((raw_data * camera_cf) / camera_qe).astype(np.uint16)
         
         if perform_decon:
-            from clij2fft.richardson_lucy import richardson_lucy_nc, getlib
-            import cupy as cp
-            lib = getlib()
-            slices = Slicer(raw_data,
-                            crop_size=(decon_chunks,raw_data.shape[1],raw_data.shape[2]),
-                            overlap=(64,0,0),
-                            batch_size=1,
-                            pad=True)
+            from pycudadecon import decon
             
-            for crop, source, destination in tqdm(slices,leave=False):             
-                raw_data[destination] = richardson_lucy_nc(img=crop,
-                                                        psf=psf_data,
-                                                        numiterations=decon_iterations,
-                                                        regularizationfactor=1e-4,
-                                                        lib=lib)[source].astype(np.uint16)
-                cp.clear_memo()
-                cp._default_memory_pool.free_all_blocks()
+            data_padded, pad_z_before, pad_z_after = pad_to_nearest_16_z_reflect(raw_data)
+            del raw_data
+            gc.collect()
+            data_decon_padded = np.zeros_like(data_padded)
+            
+            slices = Slicer(data_padded,
+                crop_size=(decon_chunks,1600,1600),
+                overlap=(64,64,64),
+                batch_size=1,
+                pad=True)
+
+            for crop, source, destination in slices: 
+                data_decon_padded[destination] = decon(
+                        images = crop,
+                        psf = psf_data,
+                        dzpsf=0.400,
+                        dxpsf=.115,
+                        dzdata=.400,
+                        dxdata=.115,
+                        wavelength=520,
+                        na=1.3,
+                        nimm=1.4,
+                        n_iters=decon_iterations,
+                        cleanup_otf=True,
+                        napodize=30,
+                        skewed_decon=True)[source]
+                
+            data_decon = remove_padding_z(data_decon_padded,pad_z_before,pad_z_after)
+            del data_padded, data_decon_padded
+            gc.collect()
+
+            
+            
+            # from clij2fft.richardson_lucy import richardson_lucy_nc, getlib
+            # import cupy as cp
+            # lib = getlib()
+            # slices = Slicer(raw_data,
+            #                 crop_size=(decon_chunks,raw_data.shape[1],raw_data.shape[2]),
+            #                 overlap=(64,0,0),
+            #                 batch_size=1,
+            #                 pad=True)
+            
+            # for crop, source, destination in tqdm(slices,leave=False):             
+            #     raw_data[destination] = richardson_lucy_nc(img=crop,
+            #                                             psf=psf_data,
+            #                                             numiterations=decon_iterations,
+            #                                             regularizationfactor=1e-4,
+            #                                             lib=lib)[source].astype(np.uint16)
+            #     cp.clear_memo()
+            #     cp._default_memory_pool.free_all_blocks()
         
-        temp_deskew = deskew(raw_data).astype(np.uint16)
+        temp_deskew = deskew(data_decon).astype(np.uint16)
         
         if crop_start and crop_end:
             crop_deskew = temp_deskew[:,overlap_size:-overlap_size,:]
