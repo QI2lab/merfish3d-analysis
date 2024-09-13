Actually fix dynamic experimental order calculation

examples/human_olfactorybulb/convert_to_qi2lab.py
@@ -23,7 +23,7 @@ from itertools import compress
 
 def convert_data():
     # root data folder
-    root_path = Path(r"/mnt/data/qi2lab/20240823_OB_22bit_2")
+    root_path = Path(r"/mnt/data/qi2lab/20240317_OB_MERFISH_7")
 
     # load codebook
     # --------------
@@ -95,7 +95,7 @@ def convert_data():
     datastore = qi2labDataStore(datastore_path)
     datastore.num_rounds = num_rounds
     datastore.codebook = codebook
-    datastore.channels_in_data = ["alexa488", "atto565", "alxa647"]
+    datastore.channels_in_data = ["alexa488", "atto565", "alexa647"]
     datastore.experiment_order = experiment_order
     datastore.num_tiles = num_tiles
     datastore.microscope_type = "3D"
@@ -128,12 +128,7 @@ def convert_data():
                 / Path(root_name + "_r"+str(round_idx+1).zfill(4)+"_tile"+str(tile_idx).zfill(4)+"_1")
                 / Path(root_name + "_r"+str(round_idx+1).zfill(4)+"_tile"+str(tile_idx).zfill(4)+"_NDTiffStack.tif")
             )
-            if round_idx == 0 and tile_idx == 0:
-                image_path = (
-                root_path
-                / Path(root_name + "_r"+str(round_idx+1).zfill(4)+"_tile"+str(tile_idx).zfill(4)+"_2")
-                / Path(root_name + "_r"+str(round_idx+1).zfill(4)+"_tile"+str(tile_idx).zfill(4)+"_NDTiffStack.tif")
-            )
+
             raw_image = imread(image_path)
             raw_image = np.swapaxes(raw_image,0,1)
             if channel_order == "reversed":
@@ -194,7 +189,7 @@ def convert_data():
             
             # write second readout channel and metadata
             datastore.save_local_corrected_image(
-                np.squeeze(raw_image[2 :]),
+                np.squeeze(raw_image[2,:]).astype(np.uint16),
                 tile=tile_idx,
                 psf_idx=2,
                 gain_correction=True,

src/merfish3danalysis/qi2labDataStore.py
@@ -285,9 +285,10 @@ class qi2labDataStore:
             for idx in range(len(self._channels_in_data)):
                 channel_list.append(str(self._channels_in_data[idx]))
             self._experiment_order = pd.DataFrame(
-                value.astype(np.int8),
-                columns=[channel_list],
-            )
+                value,
+                columns=channel_list,
+                dtype='int64'
+            )            
 
         zattrs_path = self._calibrations_zarr_path / Path(".zattrs")
         calib_zattrs = self._load_from_json(zattrs_path)
@@ -299,7 +300,7 @@ class qi2labDataStore:
         calib_zattrs["num_round"] = self._num_rounds
         self._save_to_json(calib_zattrs, zattrs_path)
 
-        self._num_bits = int(np.max(value[-1, 1:]))
+        self._num_bits = int(np.max(value[:, 1:]))
         calib_zattrs = self._load_from_json(zattrs_path)
         calib_zattrs["num_bits"] = self._num_bits
         self._save_to_json(calib_zattrs, zattrs_path)
@@ -361,7 +362,7 @@ class qi2labDataStore:
                     calib_zattrs["global_normalization_vector"], dtype=np.float32
                 )
                 return value
-            except Exception as e:
+            except Exception:
                 print("Global normalization vector not calculated.")
                 return None
         else:
@@ -390,7 +391,7 @@ class qi2labDataStore:
                     calib_zattrs["global_background_vector"], dtype=np.float32
                 )
                 return value
-            except Exception as e:
+            except Exception:
                 print("Global background vector not calculated.")
                 return None
         else:   
@@ -1196,24 +1197,21 @@ class qi2labDataStore:
                 readout_bit_path = readout_tile_path / Path(bit_id + ".zarr")
                 readout_bit_path.mkdir()
                 readout_bit_attrs_path = readout_bit_path / Path(".zattrs")
-                fiducial_channel = self._channels_in_data[0]
-                readout_one_channel = self._channels_in_data[1]
+                fiducial_channel = str(self._channels_in_data[0])
+                readout_one_channel = str(self._channels_in_data[1])
+                
                 if len(self._channels_in_data)==3:
-                    readout_two_channel = self._channels_in_data[2]
-                    row = self._experiment_order[
-                        (self._experiment_order[str(readout_one_channel)] == (bit_idx + 1))
-                        | (
-                            self._experiment_order[str(readout_two_channel)]
-                            == (bit_idx + 1)
-                        )
-                    ]
+                    readout_two_channel = str(self._channels_in_data[2])
+                    condition_one = self._experiment_order[readout_one_channel] == (bit_idx + 1)
+                    condition_two = self._experiment_order[readout_two_channel] == (bit_idx + 1)
+                    combined_condition = condition_one | condition_two
+
                 else:
-                    row = self._experiment_order[
-                        (self._experiment_order[str(readout_one_channel)] == (bit_idx + 1))
-                    ]
+                    combined_condition = self._experiment_order[readout_one_channel] == (bit_idx + 1)
+                matching_rows = self._experiment_order.loc[combined_condition]
 
                 bit_attrs = {
-                    "round_linker": int(row[str(fiducial_channel)].values[0]),
+                    "round_linker": int(matching_rows[fiducial_channel].values[0])
                 }
                 self._save_to_json(bit_attrs, readout_bit_attrs_path)
         except Exception as e: