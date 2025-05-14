0390e0c9389bf4f0dbd49eebd49b8cab9012a924
mspend <mauridobbs@gmail.com>
2025-05-14 00:00:28 +0000

    initial commit of code for decoding just one tile

---
+++ examples/bioprotean_smFISH/smFISH_decoding.py
@@ -0,0 +1,37 @@
+from merfish3danalysis.qi2labDataStore import qi2labDataStore
+from merfish3danalysis.PixelDecoder import PixelDecoder
+from pathlib import Path
+# import numpy as np
+
+root_path = Path(r"/data/smFISH/02202025_Bartelle_control_smFISH_TqIB")
+
+# initialize datastore
+datastore_path = root_path / Path(r"qi2labdatastore")
+datastore = qi2labDataStore(datastore_path)
+merfish_bits = datastore.num_bits
+
+# initialize decodor class
+decoder = PixelDecoder(
+    datastore=datastore, 
+    use_mask=False, 
+    merfish_bits=merfish_bits, 
+    verbose=1
+)
+
+# decode one tile
+decoder.decode_one_tile(
+    tile_idx=0,  # Specify the tile index
+    display_results=True,  # Set to True to visualize results in Napari
+    lowpass_sigma=(3, 1, 1),  # Lowpass filter sigma
+    magnitude_threshold=0.9,  # L2-norm threshold
+    minimum_pixels=3.0,  # Minimum number of pixels for a barcode
+    use_normalization=True,  # Use normalization
+    ufish_threshold=0.5  # Ufish threshold
+)
+
+# # access decoded image
+# decoded_image = decoder._decoded_image
+# print(decoded_image)
+
+# # Save barcodes
+# decoder._save_barcodes()
