11e31a82d57a752626ac2418ed01ddb23bef9bcf
Doug Shepherd <dpshepherd@gmail.com>
2025-10-08 11:26:30 -0700

    Update __init__.py

--- src/merfish3danalysis/__init__.py
+++ src/merfish3danalysis/__init__.py
@@ -5,11 +5,6 @@ This package provides tools for processing 3D MERFISH data using GPU
 acceleration.
 """
 
-__version__ = "0.7.0"
+__version__ = "0.7.6"
 __author__ = "Douglas Shepherd"
 __email__ = "douglas.shepherd@asu.edu"
-
-# from .utils import dataio, imageprocessing, registration, rlgc
-# from .DataRegistration import DataRegistration
-# from .PixelDecoder import PixelDecoder
-# from .qi2labDataStore import qi2labDataStore
