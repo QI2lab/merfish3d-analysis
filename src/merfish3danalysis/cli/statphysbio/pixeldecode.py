3819386a7638e9413c7153d3908578c65dfc6a9e
Doug Shepherd <dpshepherd@gmail.com>
2025-10-13 14:57:09 -0700

    Update pixeldecode.py

--- src/merfish3danalysis/cli/statphysbio/pixeldecode.py
+++ src/merfish3danalysis/cli/statphysbio/pixeldecode.py
@@ -17,8 +17,8 @@ app.pretty_exceptions_enable = False
 def decode_pixels(
     root_path: Path,
     minimum_pixels_per_RNA: int = 2,
-    distance_threshold: float = 1.0,
-    ufish_threshold: float = 0.1,
+    distance_threshold: float = .5176,
+    ufish_threshold: float = 0.2,
     magnitude_threshold: tuple[float,float]  =[0.9, 10.0],
     fdr_target: float = .05,
     smFISH: bool = False,
@@ -31,12 +31,16 @@ def decode_pixels(
         path to experiment
     minimum_pixels_per_RNA : int, default 2
         minimum pixels with same barcode ID required to call a spot.
-    ufish_threshold : float, default 0.1
+    distance_threshold: float, default 0.5176
+        maximum allowed Euclidean distance threshold between a pixel trace and codeword.
+    ufish_threshold : float, default 0.2
         threshold to accept ufish prediction.
-    magnitude_threshold: Sequence[float], default (1.1,10.0)
+    magnitude_threshold: tuple[float,float], default (0.9,10.0)
         minimum magnitude across all normalized bits required to accept a spot.
     fdr_target : float, default .05
         false discovery rate (FDR) target.
+    smFISH: bool, default = False
+        Run in smFISH mode. Overrides distance threshold to 1.0.
     """
 
     # initialize datastore
@@ -44,7 +48,6 @@ def decode_pixels(
     datastore = qi2labDataStore(datastore_path)
     merfish_bits = datastore.num_bits
     
-
     # initialize decodor class
     decoder = PixelDecoder(
         datastore=datastore, 
@@ -54,6 +57,7 @@ def decode_pixels(
         smFISH=smFISH
     )
     if smFISH:
+        distance_threshold = 1.0
         decoder._distance_threshold = distance_threshold
 
     decoder.optimize_normalization_by_decoding(
