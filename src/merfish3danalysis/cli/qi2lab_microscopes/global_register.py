c720d6b41fa7c1b3bc6bd93f65b8c98cdefdf4f6
dpshepherd <dpshepherd@gmail.com>
2025-10-21 22:23:28 -0700

    diagnose tifffile issue

--- src/merfish3danalysis/cli/qi2lab_microscopes/global_register.py
+++ src/merfish3danalysis/cli/qi2lab_microscopes/global_register.py
@@ -45,125 +45,125 @@ def global_register_data(
     datastore_path = root_path / Path(r"qi2labdatastore")
     datastore = qi2labDataStore(datastore_path)
 
-    # load tile positions
-    for tile_idx, tile_id in enumerate(datastore.tile_ids):
-        round_id = datastore.round_ids[0]
-        tile_position_zyx_um = datastore.load_local_stage_position_zyx_um(
-            tile_id, round_id
-        )
+    # # load tile positions
+    # for tile_idx, tile_id in enumerate(datastore.tile_ids):
+    #     round_id = datastore.round_ids[0]
+    #     tile_position_zyx_um = datastore.load_local_stage_position_zyx_um(
+    #         tile_id, round_id
+    #     )
 
-    # convert local tiles from first round to multiscale spatial images
-    msims = []
-    for tile_idx, tile_id in enumerate(tqdm(datastore.tile_ids, desc="tile")):
-        round_id = datastore.round_ids[0]
+    # # convert local tiles from first round to multiscale spatial images
+    # msims = []
+    # for tile_idx, tile_id in enumerate(tqdm(datastore.tile_ids, desc="tile")):
+    #     round_id = datastore.round_ids[0]
 
-        voxel_zyx_um = datastore.voxel_size_zyx_um
+    #     voxel_zyx_um = datastore.voxel_size_zyx_um
 
-        scale = {"z": voxel_zyx_um[0], "y": voxel_zyx_um[1], "x": voxel_zyx_um[2]}
+    #     scale = {"z": voxel_zyx_um[0], "y": voxel_zyx_um[1], "x": voxel_zyx_um[2]}
 
-        tile_position_zyx_um, affine_zyx_px = datastore.load_local_stage_position_zyx_um(
-            tile_id, round_id
-        )
+    #     tile_position_zyx_um, affine_zyx_px = datastore.load_local_stage_position_zyx_um(
+    #         tile_id, round_id
+    #     )
         
-        tile_grid_positions = {
-            "z": np.round(tile_position_zyx_um[0], 2),
-            "y": np.round(tile_position_zyx_um[1], 2),
-            "x": np.round(tile_position_zyx_um[2], 2),
-        }
+    #     tile_grid_positions = {
+    #         "z": np.round(tile_position_zyx_um[0], 2),
+    #         "y": np.round(tile_position_zyx_um[1], 2),
+    #         "x": np.round(tile_position_zyx_um[2], 2),
+    #     }
 
-        im_data = datastore.load_local_registered_image(
-            tile=tile_id, round=round_id, return_future=False
-        )
+    #     im_data = datastore.load_local_registered_image(
+    #         tile=tile_id, round=round_id, return_future=False
+    #     )
 
-        sim = si_utils.get_sim_from_array(
-            da.expand_dims(im_data, axis=0),
-            dims=("c", "z", "y", "x"),
-            scale=scale,
-            translation=tile_grid_positions,
-            affine=affine_zyx_px,
-            transform_key="stage_metadata",
-        )
+    #     sim = si_utils.get_sim_from_array(
+    #         da.expand_dims(im_data, axis=0),
+    #         dims=("c", "z", "y", "x"),
+    #         scale=scale,
+    #         translation=tile_grid_positions,
+    #         affine=affine_zyx_px,
+    #         transform_key="stage_metadata",
+    #     )
 
-        msim = msi_utils.get_msim_from_sim(sim, scale_factors=[])
-        msims.append(msim)
-        del im_data
-        gc.collect()
+    #     msim = msi_utils.get_msim_from_sim(sim, scale_factors=[])
+    #     msims.append(msim)
+    #     del im_data
+    #     gc.collect()
         
-    # perform registration in three steps, from most downsampling to least.
-    with dask.config.set(**{"array.slicing.split_large_chunks": False}):
-        with dask.diagnostics.ProgressBar():
-            _ = registration.register(
-                msims,
-                reg_channel_index=0,
-                transform_key="stage_metadata",
-                new_transform_key="affine_registered",
-                registration_binning={"z": 3, "y": 6, "x": 6},
-                post_registration_do_quality_filter=True,
-                n_parallel_pairwise_regs=40
-            )
+    # # perform registration in three steps, from most downsampling to least.
+    # with dask.config.set(**{"array.slicing.split_large_chunks": False}):
+    #     with dask.diagnostics.ProgressBar():
+    #         _ = registration.register(
+    #             msims,
+    #             reg_channel_index=0,
+    #             transform_key="stage_metadata",
+    #             new_transform_key="affine_registered",
+    #             registration_binning={"z": 3, "y": 6, "x": 6},
+    #             post_registration_do_quality_filter=True,
+    #             n_parallel_pairwise_regs=40
+    #         )
 
-    # extract and save transformations into datastore
-    for tile_idx, msim in enumerate(msims):
-        affine = msi_utils.get_transform_from_msim(
-            msim, transform_key="affine_registered"
-        ).data.squeeze()
-        affine = np.round(affine, 2)
-        origin = si_utils.get_origin_from_sim(
-            msi_utils.get_sim_from_msim(msim), asarray=True
-        )
-        spacing = si_utils.get_spacing_from_sim(
-            msi_utils.get_sim_from_msim(msim), asarray=True
-        )
+    # # extract and save transformations into datastore
+    # for tile_idx, msim in enumerate(msims):
+    #     affine = msi_utils.get_transform_from_msim(
+    #         msim, transform_key="affine_registered"
+    #     ).data.squeeze()
+    #     affine = np.round(affine, 2)
+    #     origin = si_utils.get_origin_from_sim(
+    #         msi_utils.get_sim_from_msim(msim), asarray=True
+    #     )
+    #     spacing = si_utils.get_spacing_from_sim(
+    #         msi_utils.get_sim_from_msim(msim), asarray=True
+    #     )
 
-        datastore.save_global_coord_xforms_um(
-            affine_zyx_um=affine,
-            origin_zyx_um=origin,
-            spacing_zyx_um=spacing,
-            tile=tile_idx,
-        )
+    #     datastore.save_global_coord_xforms_um(
+    #         affine_zyx_um=affine,
+    #         origin_zyx_um=origin,
+    #         spacing_zyx_um=spacing,
+    #         tile=tile_idx,
+    #     )
 
-    # perform and save downsampled fusion
-    with dask.config.set(**{"array.slicing.split_large_chunks": False}):
-        fused_sim = fusion.fuse(
-            [msi_utils.get_sim_from_msim(msim, scale="scale0") for msim in msims],
-            transform_key="affine_registered",
-            output_spacing={
-                "z": voxel_zyx_um[0],
-                "y": voxel_zyx_um[1] * np.round(voxel_zyx_um[0] / voxel_zyx_um[1], 1),
-                "x": voxel_zyx_um[2] * np.round(voxel_zyx_um[0] / voxel_zyx_um[2], 1),
-            },
-            output_chunksize=512,
-            overlap_in_pixels=64,
-        )
+    # # perform and save downsampled fusion
+    # with dask.config.set(**{"array.slicing.split_large_chunks": False}):
+    #     fused_sim = fusion.fuse(
+    #         [msi_utils.get_sim_from_msim(msim, scale="scale0") for msim in msims],
+    #         transform_key="affine_registered",
+    #         output_spacing={
+    #             "z": voxel_zyx_um[0],
+    #             "y": voxel_zyx_um[1] * np.round(voxel_zyx_um[0] / voxel_zyx_um[1], 1),
+    #             "x": voxel_zyx_um[2] * np.round(voxel_zyx_um[0] / voxel_zyx_um[2], 1),
+    #         },
+    #         output_chunksize=512,
+    #         overlap_in_pixels=64,
+    #     )
 
-        fused_msim = msi_utils.get_msim_from_sim(fused_sim, scale_factors=[])
-        affine = msi_utils.get_transform_from_msim(
-            fused_msim, transform_key="affine_registered"
-        ).data.squeeze()
-        origin = si_utils.get_origin_from_sim(
-            msi_utils.get_sim_from_msim(fused_msim), asarray=True
-        )
-        spacing = si_utils.get_spacing_from_sim(
-            msi_utils.get_sim_from_msim(fused_msim), asarray=True
-        )
+    #     fused_msim = msi_utils.get_msim_from_sim(fused_sim, scale_factors=[])
+    #     affine = msi_utils.get_transform_from_msim(
+    #         fused_msim, transform_key="affine_registered"
+    #     ).data.squeeze()
+    #     origin = si_utils.get_origin_from_sim(
+    #         msi_utils.get_sim_from_msim(fused_msim), asarray=True
+    #     )
+    #     spacing = si_utils.get_spacing_from_sim(
+    #         msi_utils.get_sim_from_msim(fused_msim), asarray=True
+    #     )
 
-        del fused_msim
+    #     del fused_msim
 
-        datastore.save_global_fidicual_image(
-            fused_image=fused_sim.data.compute(scheduler="threads", num_workers=12),
-            affine_zyx_um=affine,
-            origin_zyx_um=origin,
-            spacing_zyx_um=spacing,
-        )
+    #     datastore.save_global_fidicual_image(
+    #         fused_image=fused_sim.data.compute(scheduler="threads", num_workers=12),
+    #         affine_zyx_um=affine,
+    #         origin_zyx_um=origin,
+    #         spacing_zyx_um=spacing,
+    #     )
 
-        del fused_sim
-        gc.collect()
+    #     del fused_sim
+    #     gc.collect()
 
-    # update datastore state
-    datastore_state = datastore.datastore_state
-    datastore_state.update({"GlobalRegistered": True})
-    datastore_state.update({"Fused": True})
-    datastore.datastore_state = datastore_state
+    # # update datastore state
+    # datastore_state = datastore.datastore_state
+    # datastore_state.update({"GlobalRegistered": True})
+    # datastore_state.update({"Fused": True})
+    # datastore.datastore_state = datastore_state
     
     # write max projection OME-TIFF for cellpose GUI
     if create_max_proj_tiff:
