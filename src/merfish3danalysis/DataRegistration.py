80186a5882de7141e071cef91e5db9d27a6ec9e1
dpshepherd <dpshepherd@gmail.com>
2026-07-29 13:27:44 -0700

    switch to loky

--- src/merfish3danalysis/DataRegistration.py
+++ src/merfish3danalysis/DataRegistration.py
@@ -1698,7 +1698,7 @@ class DataRegistration:
         if self._verbose >= 1:
             print(time_stamp(), "Starting global fiducial fusion.")
         fusion_start_time = timeit.default_timer()
-        fusion_threads = max(1, 4 * (os.cpu_count() or 1))
+        fusion_workers = max(1, os.cpu_count() or 1)
         fused_msim = fusion.fuse(
             images=msims,
             transform_key=self._global_registration_config.new_transform_key,
@@ -1710,10 +1710,10 @@ class DataRegistration:
             },
             batch_options={
                 "batch_func": misc_utils.process_batch_using_joblib,
-                "n_batch": fusion_threads,
+                "n_batch": 4 * fusion_workers,
                 "batch_func_kwargs": {
-                    "n_jobs": fusion_threads,
-                    "backend": "threading",
+                    "n_jobs": fusion_workers,
+                    "backend": "loky",
                 },
             },
             backend="numpy",
